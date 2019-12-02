582e3166cd5e28bd814354a0714fbdf925a9a762
Natacha Romano <natacha.romano@valtech.com>
2019-12-02 15:06:19 -0300

    Updates

---
+++ precache-manifest.ee69cbba6018a8b61d276bb9ae5d029b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c60b593fa819cf6b104028309c801fdf",
+    "url": "/artists-info/index.html"
+  },
+  {
+    "revision": "dfc16891c996c4a0392b",
+    "url": "/artists-info/static/css/main.0a610c3b.chunk.css"
+  },
+  {
+    "revision": "f2c33a6ae4a8dd21d681",
+    "url": "/artists-info/static/js/2.66b98424.chunk.js"
+  },
+  {
+    "revision": "dfc16891c996c4a0392b",
+    "url": "/artists-info/static/js/main.9c468a2c.chunk.js"
+  },
+  {
+    "revision": "adfcdd2212e551d91dda",
+    "url": "/artists-info/static/js/runtime-main.67f7081d.js"
+  },
+  {
+    "revision": "d872412b07542e03c8224558b5190dfc",
+    "url": "/artists-info/static/media/music.d872412b.jpg"
+  }
+]);
